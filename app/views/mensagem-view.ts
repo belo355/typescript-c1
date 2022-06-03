dda813f4c6cb9ad9ea5fd2520772c87fd9b37dc0
edilson <edilson.belo@live.com>
2022-06-03 00:57:20 -0300

    update

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -2,7 +2,7 @@ import { View } from "./view.js";
 
 export class MenssagemView extends View<string> {
 
-  template(model: string): string {
+  protected template(model: string): string {
     return `
       <p class="alert alert-success">${model}</p>
     `
